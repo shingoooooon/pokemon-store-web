7f57f835a1c216b31fc56102b2601cc027787491
shingo kubomura <skksb75@gmail.com>
2023-08-07 10:27:47 +0700

    feat: :tada: initial commit

---
+++ components/common/AppBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <NuxtLink to="/">
+      <h1>Adventure Store</h1>
+    </NuxtLink>
+    <NuxtLink to="/cart">Cart</NuxtLink>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <app-bar></app-bar>
+</template>
+
+<style scoped></style>
